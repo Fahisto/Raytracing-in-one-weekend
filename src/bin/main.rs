876af9e090711ada17e4ced6efe202ae0b261880
Grigoryi <fahisto.fahisto@yandex.ru>
2019-07-30 11:12:07 +0300

    Chaapter 6 completed

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,13 @@
 use ppm::*;
-use ppm::{Sphere, HitableList, Hitable};
+use ppm::{Hitable, HitableList, Sphere};
+use rand::*;
 use std::fs::*;
 use std::io::*;
 
 fn main() {
     let nx: i32 = 200;
     let ny: i32 = 100;
+    let ns: i32 = 100;
     const COEFF: f32 = 255.99;
 
     let mut file_handle = File::create("my_ppm.ppm").expect("Failed to create file");
@@ -15,26 +17,26 @@ fn main() {
         .write(&contents.as_bytes())
         .expect("Failed to write contents");
 
-    let lower_left_corner = Vec3(-2.0, -1.0, -1.0);
-    let hor = Vec3(4.0, 0.0, 0.0);
-    let ver = Vec3(0.0, 2.0, 0.0);
-    let orig = Vec3::zero();
-    let hitable_list = HitableList::new_filled_list(
-        vec![
-            Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5),
-            Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0),
+    let hitable_list = HitableList::new_filled_list(vec![
+        Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5),
+        Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0),
     ]);
-
+    let camera = Camera::new();
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-
-            let ray = Ray::new(orig, lower_left_corner + hor * u + ver * v);
-
-            let color = ppm::color(ray, &hitable_list);
-            //println!("{}", color);
-
+            let mut color = Vec3::zero();
+            for _ in 0..ns {
+                let rand1: f32 = rand::thread_rng().gen();
+                let rand2: f32 = rand::thread_rng().gen();
+                let u = ((i as f32) + rand1) / nx as f32;
+                let v = ((j as f32) + rand2) / ny as f32;
+                let ray = camera.get_ray(u, v);
+                let point = ray.point_at_parameter(2.0);
+                color += ppm::color(ray, &hitable_list);
+                //println!("{}", color);
+            }
+            color /= ns as f32;
+            color = Vec3(color.0.sqrt(), color.1.sqrt(), color.2.sqrt());
             let ir: i32 = (COEFF * color.0) as i32;
             let ig: i32 = (COEFF * color.1) as i32;
             let ib: i32 = (COEFF * color.2) as i32;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use rand::*;
 use std::fmt;
 use std::ops::*;
 use std::vec::Vec;
@@ -42,6 +43,10 @@ impl Vec3 {
 
     pub fn magn(&self) -> f32 {
         (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
+    }
+
+    pub fn sqr_magn(&self) -> f32 {
+        self.magn() * self.magn()
     }
 
     pub fn to_unit(self) -> Vec3 {
@@ -89,13 +94,11 @@ impl Mul<f32> for Vec3 {
     }
 }
 
-impl Div<f32> for Vec3{
+impl Div<f32> for Vec3 {
     type Output = Self;
 
-    fn div(self, other: f32) -> Self
-    {
-        if other == 0.0
-        {
+    fn div(self, other: f32) -> Self {
+        if other == 0.0 {
             panic!("Cant divide by zero");
         }
 
@@ -103,13 +106,22 @@ impl Div<f32> for Vec3{
     }
 }
 
-impl Div for Vec3{
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other: f32) {
+        if other == 0.0 {
+            panic!("You cant divide by zero");
+        }
+        self.0 /= other;
+        self.1 /= other;
+        self.2 /= other;
+    }
+}
+
+impl Div for Vec3 {
     type Output = Self;
 
-    fn div(self, other: Self) -> Self
-    {
-        if other.0 == 0.0 || other.1 == 0.0 || other.2 == 0.0
-        {
+    fn div(self, other: Self) -> Self {
+        if other.0 == 0.0 || other.1 == 0.0 || other.2 == 0.0 {
             panic!("Cant divide by zero");
         }
 
@@ -124,115 +136,122 @@ impl Clone for Vec3 {
     }
 }
 
-pub struct HitInfo
-{
+pub struct HitInfo {
     hit_param: f32,
     hit_point: Vec3,
     normal: Vec3,
 }
 
-impl HitInfo
-{
-    pub fn new() -> HitInfo
-    {
-        HitInfo{ hit_param: 0.0, hit_point: Vec3::zero(), normal: Vec3::zero()}
+impl HitInfo {
+    pub fn new() -> HitInfo {
+        HitInfo {
+            hit_param: 0.0,
+            hit_point: Vec3::zero(),
+            normal: Vec3::zero(),
+        }
     }
 }
 
-pub trait Hitable{
+pub trait Hitable {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, info: &mut HitInfo) -> bool;
 }
 
-pub struct Sphere{
+pub struct Sphere {
     center: Vec3,
     radius: f32,
 }
 
-impl Sphere{
-    pub fn new(center: Vec3, radius: f32) -> Sphere
-    {
-        Sphere {center, radius}
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Sphere {
+        Sphere { center, radius }
     }
 
-    pub fn unit() -> Sphere{
-        Sphere{center: Vec3(0.0, 0.0, 0.0), radius: 1.0}
+    pub fn unit_from_origin() -> Sphere {
+        Sphere {
+            center: Vec3(0.0, 0.0, 0.0),
+            radius: 1.0,
+        }
+    }
+
+    pub fn unit_from_point(center: Vec3) -> Sphere {
+        Sphere {
+            center,
+            radius: 1.0,
+        }
     }
 }
 
-impl Hitable for Sphere
-{
-    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, info: &mut HitInfo) -> bool
-    {
+impl Hitable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, info: &mut HitInfo) -> bool {
         let oc = ray.get_origin() - self.center;
         let a = Vec3::dot(ray.get_dir(), ray.get_dir());
         let b = 2.0 * Vec3::dot(oc, ray.get_dir());
         let c = Vec3::dot(oc, oc) - self.radius * self.radius;
         let discr = b * b - 4.0 * a * c;
 
-        if discr > 0.0
-        {
+        if discr > 0.0 {
             let mut temp = (-b - discr.sqrt()) / (2.0 * a);
-            if temp > t_min && temp < t_max{
+            if temp > t_min && temp < t_max {
                 info.hit_param = temp;
                 info.hit_point = ray.point_at_parameter(temp);
                 info.normal = (info.hit_point - self.center) / self.radius;
                 return true;
-            }
-            else
-            {
+            } else {
                 temp = (-b + discr.sqrt()) / (2.0 * a);
-                if temp > t_min && temp < t_max
-                {
+                if temp > t_min && temp < t_max {
                     info.hit_param = temp;
                     info.hit_point = ray.point_at_parameter(temp);
                     info.normal = (info.hit_point - self.center) / self.radius;
                     return true;
-                }
-                else
-                {
+                } else {
                     return false;
                 }
                 return false;
             }
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 }
 
-pub struct HitableList<T> where T:Hitable
+pub struct HitableList<T>
+where
+    T: Hitable,
 {
     list: Vec<T>,
     list_size: i32,
 }
 
-impl<T> HitableList<T> where T: Hitable
+impl<T> HitableList<T>
+where
+    T: Hitable,
 {
-    pub fn new() -> HitableList<T>
-    {
-        HitableList::<T> { list: Vec::<T>::new(), list_size: 0}
+    pub fn new() -> HitableList<T> {
+        HitableList::<T> {
+            list: Vec::<T>::new(),
+            list_size: 0,
+        }
     }
 
-    pub fn new_filled_list(items: Vec<T>) -> HitableList<T>
-    {
+    pub fn new_filled_list(items: Vec<T>) -> HitableList<T> {
         let list_size = items.len() as i32;
-        HitableList::<T> { list: items, list_size}
+        HitableList::<T> {
+            list: items,
+            list_size,
+        }
     }
 }
 
-impl<T> Hitable for HitableList<T> where T: Hitable
+impl<T> Hitable for HitableList<T>
+where
+    T: Hitable,
 {
-    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, info: &mut HitInfo) -> bool
-    {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, info: &mut HitInfo) -> bool {
         let mut temp_info: HitInfo = HitInfo::new();
         let mut hit_any: bool = false;
         let mut closest_yet: f32 = t_max;
-        for item in self.list.iter()
-        {
-            if item.hit(ray, t_min, closest_yet, &mut temp_info)
-            {
+        for item in self.list.iter() {
+            if item.hit(ray, t_min, closest_yet, &mut temp_info) {
                 hit_any = true;
                 closest_yet = temp_info.hit_param;
                 info.hit_param = temp_info.hit_param;
@@ -244,16 +263,57 @@ impl<T> Hitable for HitableList<T> where T: Hitable
     }
 }
 
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            lower_left_corner: Vec3(-2.0, -1.0, -1.0),
+            horizontal: Vec3(4.0, 0.0, 0.0),
+            vertical: Vec3(0.0, 2.0, 0.0),
+            origin: Vec3(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + self.horizontal * u + self.vertical * v - self.origin,
+        )
+    }
+}
+
+pub trait Scattering{
+    fn scatter(ray_in: Ray, info: HitInfo, attenuation: Vec3, scattered: Ray) -> bool;
+}
+
+pub struct Lambertian
+{
+    albedo: Vec3,
+}
+
+impl Lambertian
+{
+    pub fn new(albedo: Vec3) -> Lambertian
+    {
+        Lambertian{albedo}
+    }
+}
+
+
+
 pub fn color<T: Hitable>(ray: Ray, world: &T) -> Vec3 {
     let mut hit_info = HitInfo::new();
 
-    if(world.hit(&ray, 0.0, std::f32::MAX, &mut hit_info))
-    {
-        Vec3(hit_info.normal.0 + 1.0, hit_info.normal.1 + 1.0, hit_info.normal.2 + 1.0) * 0.5
-    }
-
-    else
-    {
+    if world.hit(&ray, 0.001, std::f32::MAX, &mut hit_info) {
+        let target = hit_info.hit_point + hit_info.normal + random_point_in_unit_sphere();
+        color(Ray::new(hit_info.hit_point, target - hit_info.hit_point), world) * 0.5
+    } else {
         let unit_dir = ray.get_dir().to_unit();
         let t = 0.5 * (unit_dir.1 + 1.0);
         Vec3(1.0, 1.0, 1.0) * (1.0 - t) + Vec3(0.5, 0.7, 1.0) * t
@@ -268,6 +328,19 @@ pub fn color<T: Hitable>(ray: Ray, world: &T) -> Vec3 {
 
         Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
     }*/
+}
+
+pub fn random_point_in_unit_sphere() -> Vec3 {
+    let mut point = Vec3::zero();
+    loop {
+        let rand1 = rand::thread_rng().gen();
+        let rand2 = rand::thread_rng().gen();
+        let rand3 = rand::thread_rng().gen();
+        point = Vec3(rand1, rand2, rand3) * 2.0 - Vec3::new(1.0, 1.0, 1.0);
+        if point.sqr_magn() < 1.0 {
+            return point;
+        }
+    }
 }
 
 pub fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
